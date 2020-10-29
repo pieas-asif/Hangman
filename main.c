01900f8880d6814f06c5e621dff14faa8f7931e7
pieas-asif <pieas.asif4@gmail.com>
2020-10-29 08:11:43 +0600

    Game completed, No Medium and Hard Database

--- main.c
+++ main.c
@@ -3,9 +3,13 @@
 #include <string.h>
 #include <unistd.h>
 #include <time.h>
+#include <ctype.h>
 
 void welcome();
 int get_word(char *, int);
+int run_game(char *, int);
+void put_into_string(char, char *);
+int is_blank_available(char *);
 
 int main(int argc, char const *argv[])
 {
@@ -57,6 +61,17 @@ int main(int argc, char const *argv[])
     word_len = (int)strlen(game_word);
     printf("WORD: %s\n", game_word);
     printf("WORD_LEN: %d\n", word_len);
+
+    check_ok = run_game(game_word, word_len);
+    if (check_ok)
+    {
+        puts("You've won the game. Run it to play again.");
+    }
+    else
+    {
+        puts("You've lost the game. Try again.");
+    }
+
     return 0;
 }
 
@@ -111,4 +126,78 @@ int get_word(char game_word[], int d)
     }
     fclose(file);
     return 1;
+}
+
+int run_game(char game_word[], int l)
+{
+    char word_guessed[60];
+    int life_left = 6;
+    int correct_guess;
+    char input_c, char_flush;
+    char blanks[l];
+
+    for (int i = 0; i < l; i++)
+    {
+        blanks[i] = '_';
+    }
+    blanks[l] = '\0';
+    word_guessed[0] = '\0';
+
+    while (life_left != 0 && is_blank_available(blanks))
+    {
+        correct_guess = 0;
+        puts("Enter a character, Try to guess the word.");
+        printf("So far you've guessed: %s\n", word_guessed);
+        printf("WORD: %s\n", blanks);
+        printf("INPUT: ");
+        scanf("%c", &input_c);
+        scanf("%c", &char_flush);
+        put_into_string(input_c, word_guessed);
+        for (int i = 0; i < l; i++)
+        {
+            if (game_word[i] == input_c && blanks[i] == '_')
+            {
+                blanks[i] = input_c;
+                correct_guess = 1;
+                printf("\n\n");
+            }
+        }
+        if (!correct_guess)
+        {
+            puts("You've guessed wrong.");
+            life_left--;
+            printf("LIFE LEFT: %d\n\n", life_left);
+        }
+    }
+    if (life_left == 0)
+    {
+        return 0;
+    }
+    else
+    {
+        return 1;
+    }
+}
+
+void put_into_string(char c, char word[])
+{
+    int i = strlen(word);
+    word[i] = toupper(c);
+    word[i + 1] = ',';
+    word[i + 2] = '\0';
+    return;
+}
+
+int is_blank_available(char word[])
+{
+    int a_blank_available = 0;
+    int l = strlen(word);
+    for (int i = 0; i < l; i++)
+    {
+        if (word[i] == '_')
+        {
+            a_blank_available = 1;
+        }
+    }
+    return a_blank_available;
 }
